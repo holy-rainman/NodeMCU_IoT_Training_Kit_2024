a42dcda42dbfd72c189255a3b5f4c1e65ec9ef96
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2024-06-04 03:10:37 +0800

    Create pwm_LED1.ino

---
+++ pwm_LED1.ino
@@ -0,0 +1,12 @@
+int led[4]={D0,D1,D2,D3};
+
+void setup() 
+{ for(int i=0;i<=3;i++) pinMode(led[i],OUTPUT);
+}
+
+void loop() 
+{ for(int i=0;i<255;i++)
+  { analogWrite(D0,i);
+    delay(5);
+  }
+}
