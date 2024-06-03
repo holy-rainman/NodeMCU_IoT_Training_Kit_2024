72e5419fbc354594744a0ad7b551421d9b82752a
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2024-06-03 17:09:43 +0800

    Update digitalOutput_sequence.ino

--- digitalOutput_sequence.ino
+++ digitalOutput_sequence.ino
@@ -1,11 +1,13 @@
 int led1=D0, led2=D1, led3=D2, led4=D3;
-void setup() 
+
+void setup() 
 { pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);  
 }
-void loop() 
+
+void loop() 
 { digitalWrite(led1, HIGH); 
   digitalWrite(led2, HIGH); 
   digitalWrite(led3, HIGH); 
