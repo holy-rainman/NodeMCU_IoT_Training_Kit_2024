966f8bd9883a342545324dc7890716bc6c0c908d
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2024-06-05 18:52:56 +0800

    Update Blynk_basic.ino

--- Blynk_basic.ino
+++ Blynk_basic.ino
@@ -1,7 +1,7 @@
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID   "TMPL6XTP0YTC1"
+#define BLYNK_TEMPLATE_ID 	 "TMPL6XTP0YTC1"
 #define BLYNK_TEMPLATE_NAME "First IoT Program"
-#define BLYNK_AUTH_TOKEN    "Xx9Xjre2Stv3E2sG2FwJL7H6ke5ZbaLk"
+#define BLYNK_AUTH_TOKEN 	 "Xx9Xjre2Stv3E2sG2FwJL7H6ke5ZbaLk"
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
@@ -10,7 +10,7 @@ char pass[] = "123456abc";
 
 void setup()
 { Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 void loop()
