92ebcc4d3ee699ae768c8990dcea9c1129f277f6
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2024-06-08 18:55:23 +0800

    Create Blynk_BlynkWrite_2.ino

---
+++ Blynk_BlynkWrite_2.ino
@@ -0,0 +1,34 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID 	"TMPL6XTP0YTC1"
+#define BLYNK_TEMPLATE_NAME "First IoT Program"
+#define BLYNK_AUTH_TOKEN 	  "Xx9Xjre2Stv3E2sG2FwJL7H6ke5ZbaLk"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char ssid[] = "UTHM@wifi";
+char pass[] = "123456abc";
+BlynkTimer timer;
+
+int status=0;
+void kelip()
+{ digitalWrite(D1,status);
+  status =! status;
+}
+
+BLYNK_WRITE(V0)
+{ int pb = param.asInt();
+  digitalWrite(D0,pb);
+}
+
+void setup()
+{ Serial.begin(9600);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  timer.setInterval(500L,kelip);
+  pinMode(D0,OUTPUT);
+  pinMode(D1,OUTPUT);
+}
+
+void loop()
+{ Blynk.run();
+  timer.run();
+}
