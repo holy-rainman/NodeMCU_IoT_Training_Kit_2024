084f6c6685607491a194d8c68a29a6ee49146ed8
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-01-05 12:08:03 +0800

    Create Send_Telegram.ino

---
+++ Send_Telegram.ino
@@ -0,0 +1,103 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "REPLACE_WITH_YOUR_TEMPLATE_ID"
+#define BLYNK_TEMPLATE_NAME "REPLACE_WITH_YOUR_TEMPLATE_NAME"
+#define BLYNK_AUTH_TOKEN "REPLACE_WITH_YOUR_AUTH TOKEN"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <UrlEncode.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+#include "DHTesp.h"
+DHTesp dht;
+
+String phoneNumber = "REPLACE_WITH_YOUR_PHONE_NUMBER"; //+60177002044
+String apiKey = "REPLACE_WITH_YOUR_APIKEY_WHATSAPP";
+
+// Initialize Telegram BOT
+#define BOTtoken "xxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxx_REPLACE_WITH_YOUR_TELEGRAM_BOT_TOKEN"  // your Bot Token (Get from Botfather)
+#define CHAT_ID "REPLACE_WITH_YOUR_CHAT_ID"
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+char ssid[] = "REPLACE_WITH_YOUR_SSID";
+char pass[] = "REPLACE_WITH_YOUR_PASSWORD";
+BlynkTimer timer;
+
+int status=0;
+void kelip()
+{ digitalWrite(D1,status);
+  status =! status;
+}
+
+BLYNK_WRITE(V0)
+{ int pb = param.asInt();
+  digitalWrite(D0,pb);
+}
+
+void getPB()
+{ int pbD5 = digitalRead(D5);
+  if(pbD5==1) { Blynk.virtualWrite(V1,"ON"); sendMessage("PB pushed!"); bot.sendMessage(CHAT_ID, "PB pushed!", ""); }
+  if(pbD5==0) Blynk.virtualWrite(V1,"OFF");
+}
+
+char dhtData[100];
+void getDHT11()
+{ float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+
+  sprintf(dhtData,"Humid: %.1f%\tTemp:%.1f",humidity,temperature);
+  Serial.println(dhtData);
+  Blynk.virtualWrite(V2,humidity);
+  Blynk.virtualWrite(V3,temperature);
+}
+
+void sendMessage(String message)
+{ // Data to send with HTTP POST
+  String url = "http://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
+  WiFiClient client;    
+  HTTPClient http;
+  http.begin(client, url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200)
+  { Serial.print("Message sent successfully");
+  }
+  else
+  { Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+}
+
+void setup()
+{ Serial.begin(9600);
+  dht.setup(D7, DHTesp::DHT11);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  timer.setInterval(500L,kelip);
+  timer.setInterval(10L,getPB);
+  timer.setInterval(2000L,getDHT11);
+  pinMode(D0,OUTPUT);
+  pinMode(D1,OUTPUT);
+  pinMode(D5,INPUT);
+
+  configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
+  client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
+}
+
+void loop()
+{ Blynk.run();
+  timer.run();
+}
